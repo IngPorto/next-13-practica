b1e7abf586f48ee970fa92230e35dc7a927e046c
ingporto <ing.portocarrero@gmail.com>
2023-02-12 01:13:20 -0500

    getServerSideProps & getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,37 @@
 import React, { useState, useEffect } from 'react';
 
-const Home = () => {
-    const [productList, setProductList] = useState([]);
 
-    useEffect(()=>{
-        window.fetch('/api/avo')
-            .then(response => response.json())
-            //.then(console.log)
-            .then(({data, length}) => {
-                setProductList(data)
-            })
-    }, [])
+/* Static Side Generated:
+* Genera problemas en el build porque intenta reconstruir la página con la información 
+* solicitada por el servicio, pero como el servicio se encuentra justamente en el mismo 
+* proyecto, y el proyecto está parado, entonces tira un error
+
+export const getStaticProps = async () => {
+    const response = await fetch('http://localhost:3000/api/avo');
+    const {data: productList } = await response.json(); // rename data to productList
+            
+    return {
+        props: {
+            productList,
+        }
+    }
+}
+*/
+
+// Server Side Render
+export const getServerSideProps = async () => {
+    const response = await fetch('http://localhost:3000/api/avo');
+    const {data: productList } = await response.json(); // rename data to productList
+            
+    return {
+        props: {
+            productList,
+        }
+    }
+}
+
+const Home = ({productList}) => {
+    //useEffect(()=>{ }, []) // Siempre se ejecuta en el navegador. Client-Site Rendering
     return (
         <div>
             <h1>Hola mundo</h1>
